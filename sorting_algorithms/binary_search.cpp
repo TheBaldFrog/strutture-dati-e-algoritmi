6a4276a315baa0229153638d0031b8eb4c22c50e
Lius <morarolivio@gmail.com>
2024-01-14 15:56:54 +0100

    binary search recursive

--- sorting_algorithms/binary_search.cpp
+++ sorting_algorithms/binary_search.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <vector>
 
+int binarySearchRecursive(int array[], int x, int low, int high)
+{
+    if (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (array[mid] == x)
+            return mid;
+
+        if (array[mid] > x)
+            return binarySearchRecursive(array, x, low, mid - 1);
+        else
+            return binarySearchRecursive(array, x, mid + 1, high);
+    }
+
+    return -1;
+}
+
 int binarySearch(std::vector<int> &input, int x)
 {
     int low = 0;
@@ -26,6 +44,15 @@ int main()
 {
     std::vector numbers = {3, 4, 5, 6, 7, 8, 9};
     int result = binarySearch(numbers, 4);
+    if (result == -1)
+        std::cout << "Not found";
+    else
+        std::cout << "Element is found at index " << result << '\n';
+
+    int array[] = {3, 4, 5, 6, 7, 8, 9};
+    int x = 4;
+    int n = sizeof(array) / sizeof(array[0]);
+    result = binarySearchRecursive(array, x, 0, n - 1);
     if (result == -1)
         std::cout << "Not found";
     else
