384a50b310eea0e8603616fac8bc230277ae5f15
Lius <morarolivio@gmail.com>
2024-01-16 21:45:56 +0100

    add dynamic array demo

---
+++ data_structures/dynamic_array/dynamic_array_demo.cpp
@@ -0,0 +1,48 @@
+#include "dynamic_array.h"
+
+void print(const DynamicArray &arr)
+{
+    if (arr.empty())
+    {
+        std::cout << "The array is empty.\n";
+        return;
+    }
+
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        std::cout << arr[i] << ' ';
+    }
+
+    std::cout << '\n';
+}
+
+int main()
+{
+    DynamicArray arr;
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        arr.push_back(static_cast<double>(i));
+    }
+
+    double sum = 0;
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        sum += arr[i];
+    }
+
+    print(arr);
+    std::cout << "Sum of array elements is: " << sum << '\n';
+
+    for (size_t i = 1; i < 10; i++)
+    {
+        print(arr);
+        arr.pop_back();
+    }
+    print(arr);
+
+    std::cout << "Array capacity: " << arr.capacity() << '\n';
+    std::cout << "Array size: " << arr.size() << '\n';
+
+    return 0;
+}
--- data_structures/dynamic_array/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "dynamic_array.h"
-
-int main()
-{
-    DynamicArray arr;
-    return 0;
-}
